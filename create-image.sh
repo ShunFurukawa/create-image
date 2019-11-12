13c99156c9af653dc348fb9efb52436552ca0abf
Shun Furukawa <furukawa9312@gmail.com>
2019-11-12 10:20:07 +0900

    :wrench: ADD Create Sample Images Shell

--- create-image.sh
+++ create-image.sh
@@ -4,3 +4,28 @@ function usage() {
   echo "Usage $0 width height"
   exit 1
 }
+
+if [ -z "$1" ]; then
+  usage
+fi
+if [ -z "$2" ]; then
+  usage
+fi
+
+width=$1
+height=$2
+point=`expr $width / 2`
+
+mkdir images
+
+for f in bmp gif jpeg jpg png tiff tif; do
+  convert -size ${width}x${height} xc:'#6969' \
+    -pointsize $point \
+    -gravity center \
+    -fill black \
+    -stroke white \
+    -draw "text 0,0 $f" images/${width}x${height}.$f
+done
+
+echo "Create Example Images! \n
+      cd images"
